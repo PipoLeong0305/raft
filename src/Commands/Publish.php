77b8337b312945438853865acc192ca202d26ffa
pipo leong <leongpipo0305@gmail.com>
2024-07-09 15:23:33 +0800

    feat: Add new Publish command class

---
+++ src/Commands/Publish.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Pipo\Raft\Commands;
+
+use CodeIgniter\CLI\BaseCommand;
+use CodeIgniter\Publisher\Publisher;
+use Throwable;
+
+class Publish extends BaseCommand
+{
+
+    /**
+     * The Command's Group
+     *
+     * @var string
+     */
+    protected $group = 'Raft';
+
+    /**
+     * The Command's Name
+     *
+     * @var string
+     */
+    protected $name = 'raft:publish';
+
+    /**
+     * The Command's Description
+     *
+     * @var string
+     */
+    protected $description = '';
+
+    /**
+     * The Command's Usage
+     *
+     * @var string
+     */
+    protected $usage = 'raft:publish [arguments] [options]';
+
+    /**
+     * The Command's Arguments
+     *
+     * @var array
+     */
+    protected $arguments = [];
+
+    /**
+     * The Command's Options
+     *
+     * @var array
+     */
+    protected $options = [];
+
+    public function run(array $params)
+    {
+        // TODO: Implement run() method.
+
+        $publisher = new Publisher( VENDORPATH . 'test/local', ROOTPATH);
+
+        try {
+
+            $publisher->publish();
+
+        } catch (Throwable $e) {
+
+            $this->showError($e);
+
+        }
+    }
+}
