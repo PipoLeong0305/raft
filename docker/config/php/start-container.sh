9f5e54567f1e03dca7a9d8d68f2bb3f5d2366568
PipoLeong <pipoleong@genwhy.com.my>
2025-05-01 01:32:25 +0800

    Enable Apache modules and switch to supervisord for service management

--- docker/config/php/start-container.sh
+++ docker/config/php/start-container.sh
@@ -2,37 +2,29 @@
 
 # Fix permissions for runtime directories
 if [ "$(id -u)" = "0" ]; then
-  chmod -R 775 /var/run/apache2 /var/lock/apache2 /var/log/apache2
-  chown -R raft:raft /var/run/apache2 /var/lock/apache2 /var/log/apache2 /var/www/html
-  
   # Print Node.js, npm, and Yarn versions
   echo "Node.js version: $(node -v)"
   echo "npm version: $(npm -v)"
   echo "Yarn version: $(yarn -v)"
   
-  # Start PHP-FPM
-  php-fpm -D
+  chmod -R 775 /var/run/apache2 /var/lock/apache2 /var/log/apache2
+  chown -R raft:raft /var/run/apache2 /var/lock/apache2 /var/log/apache2 /var/www/html
   
-  # Start Apache in the foreground
-  exec apache2ctl -D FOREGROUND
-else
-  # Already running as non-root user
-  php-fpm -D
-  exec apache2ctl -D FOREGROUND
+  if [ ! -z "$WWWUSER" ]; then
+      usermod -u $WWWUSER raft
+  fi
+  
+  if [ ! -d /.composer ]; then
+      mkdir /.composer
+  fi
+  
+  chmod -R ugo+rw /.composer
 fi
 
-if [ ! -z "$WWWUSER" ]; then
-    usermod -u $WWWUSER raft
-fi
-
-if [ ! -d /.composer ]; then
-    mkdir /.composer
-fi
-
-chmod -R ugo+rw /.composer
-
+# Handle command arguments
 if [ $# -gt 0 ]; then
     exec gosu raft "$@"
 else
+    # Start services with supervisord
     exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
 fi
